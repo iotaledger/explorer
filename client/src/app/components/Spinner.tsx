4fae508eda46e5fa3f9137701d099c0886e5ea2e
Martyn Janes <martyn@obany.com>
2020-10-26 15:18:05 +0100

    Add compact spinner style

--- client/src/app/components/Spinner.tsx
+++ client/src/app/components/Spinner.tsx
@@ -1,17 +1,22 @@
+import classNames from "classnames";
 import React, { Component, ReactNode } from "react";
 import "./Spinner.scss";
+import { SpinnerProps } from "./SpinnerProps";
 
 /**
  * Component which will display a spinner.
  */
-class Spinner extends Component {
+class Spinner extends Component<SpinnerProps> {
     /**
      * Render the component.
      * @returns The node to render.
      */
     public render(): ReactNode {
         return (
-            <div className="spinner" />
+            <div className={
+                classNames("spinner", { "spinner--compact": this.props.compact })
+            }
+            />
         );
     }
 }
---
+++ client/src/app/components/SpinnerProps.ts
@@ -0,0 +1,9 @@
+/**
+ * The props for the Spinner component.
+ */
+export interface SpinnerProps {
+    /**
+     * Show in compact mode.
+     */
+    compact?: boolean;
+}
