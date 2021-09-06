08c32af0d9b73b4058be70be68ca050cc2d4c6b0
Ruben Alvarez <ralvarez@boxfish.studio>
2021-09-06 17:44:13 +0200

    Feat: Add Footer

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,6 +1,13 @@
 import React, { Component, ReactNode } from "react";
 import logoFooter from "../../assets/logo-footer.svg";
 import { FoundationDataHelper } from "../../helpers/foundationDataHelper";
+import { ReactComponent as DiscordIcon } from "./../../assets/discord.svg";
+import { ReactComponent as GithubIcon } from "./../../assets/github.svg";
+import { ReactComponent as InstagramIcon } from "./../../assets/instagram.svg";
+import { ReactComponent as LinkedinIcon } from "./../../assets/linkedin.svg";
+import { ReactComponent as RedditIcon } from "./../../assets/reddit.svg";
+import { ReactComponent as TwitterIcon } from "./../../assets/twitter.svg";
+import { ReactComponent as YoutubeIcon } from "./../../assets/youtube.svg";
 import "./Footer.scss";
 import { FooterProps } from "./FooterProps";
 import { FooterState } from "./FooterState";
@@ -9,6 +16,51 @@ import { FooterState } from "./FooterState";
  * Component which will show the footer.
  */
 class Footer extends Component<FooterProps, FooterState> {
+    private readonly SOCIAL_LINKS = [
+        {
+            name: "Youtube",
+            icon: <YoutubeIcon />,
+            url: "https://www.youtube.com/c/iotafoundation",
+            color: "#131f36"
+        },
+        {
+            name: "GitHub",
+            icon: <GithubIcon />,
+            url: "https://github.com/iotaledger/",
+            color: "#3c4964"
+        },
+        {
+            name: "Discord",
+            icon: <DiscordIcon />,
+            url: "https://discord.iota.org/",
+            color: "#69768f"
+        },
+        {
+            name: "Twitter",
+            icon: <TwitterIcon />,
+            url: "https://twitter.com/iota",
+            color: "#8493ad"
+        },
+        {
+            name: "Reddit",
+            icon: <RedditIcon />,
+            url: "https://www.reddit.com/r/Iota/",
+            color: "#9fafca"
+        },
+        {
+            name: "LinkedIn",
+            icon: <LinkedinIcon />,
+            url: "https://www.linkedin.com/company/iotafoundation/",
+            color: "#c3d0e4"
+        },
+        {
+            name: "Instagram",
+            icon: <InstagramIcon />,
+            url: " https://www.instagram.com/iotafoundation/",
+            color: "#d1ddf0"
+        },
+    ];
+
     /**
      * Create a new instance of Footer.
      * @param props The props.
@@ -46,40 +98,58 @@ class Footer extends Component<FooterProps, FooterState> {
     public render(): ReactNode {
         return (
             <footer>
-                <div className="inner">
-                    <div className="row center">
-                        <img src={logoFooter} alt="IOTA" />
-                    </div>
-                    <div className="row">
-                        {[this.state.siteFooterSection].concat(this.state.foundationData?.footerSections ?? [])
-                            .map((section, sectionIdx) => (
-                                <section className="col fill" key={sectionIdx}>
-                                    <h3>{section.label}</h3>
-                                    <ul>
-                                        {section.items.map((info, infoIdx) => (
-                                            <li key={infoIdx}>
-                                                {FoundationDataHelper.buildLink(info.url, info.label)}
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </section>
-                            ))}
-                    </div>
-                    <hr className="sep" />
-                    <div className="row secondary">
-                        <section className="col fill line-breaks">
-                            {this.state.foundationData?.registeredAddress.value.join("\n")}
-                        </section>
-                        <section className="col fill">
-                            <ul>
-                                {this.state.foundationData?.information.map((info, infoIdx) => (
-                                    <li key={infoIdx}>
-                                        {FoundationDataHelper.createValue(info)}
-                                    </li>
+                <div className="foundation--data">
+                    <div className="inner">
+                        <div className="row">
+                            {[this.state.siteFooterSection].concat(this.state.foundationData?.footerSections ?? [])
+                                .map((section, sectionIdx) => (
+                                    <section className="col fill" key={sectionIdx}>
+                                        <h3>{section.label}</h3>
+                                        <ul>
+                                            {section.items.map((info, infoIdx) => (
+                                                <li key={infoIdx}>
+                                                    {FoundationDataHelper.buildLink(info.url, info.label)}
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </section>
                                 ))}
-                            </ul>
-                        </section>
+                        </div>
+                        <hr className="sep" />
+                        <div className="row secondary">
+                            <div className="col fill margin-t-m">
+                                <img src={logoFooter} alt="IOTA" />
+                            </div>
+                            <section className="col fill line-breaks">
+                                {this.state.foundationData?.registeredAddress.value.join("\n")}
+                            </section>
+                            <section className="col fill">
+                                <ul>
+                                    {this.state.foundationData?.information.map((info, infoIdx) => (
+                                        <li key={infoIdx}>
+                                            {FoundationDataHelper.createValue(info)}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </section>
+                        </div>
                     </div>
+
+                </div>
+                <div className="social--media__wrapper">
+                    {this.SOCIAL_LINKS.map((socialPage, socialPageID) => (
+                        <a
+                            href={socialPage.url}
+                            key={socialPageID}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={{ backgroundColor: socialPage.color }}
+                            className="social--media__item"
+                        >
+                            {socialPage.icon}
+                            <span>{socialPage.name}</span>
+                        </a>
+                    ))}
                 </div>
             </footer>
         );
