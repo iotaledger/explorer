a1b6f6c155c445a266d3ffea60f0d03134d0c0e3
evavirseda <evirseda@boxfish.studio>
2021-09-09 17:20:02 +0200

    Refine -To- scss in Transaction Payload

--- client/src/app/components/chrysalis/TransactionPayload.tsx
+++ client/src/app/components/chrysalis/TransactionPayload.tsx
@@ -265,10 +265,10 @@ class TransactionPayload extends AsyncComponent<TransactionPayloadProps, Transac
                             {this.state.outputs.map((output, idx) => (
                                 <React.Fragment key={idx}>
                                     <div
-                                        className="card--content__input card--content__flex_between"
+                                        className="card--content__input "
                                         onClick={() => this.setState({ showOutputDetails: this.state.showOutputDetails === idx ? -1 : idx })}
                                     >
-                                        <div className={classNames("margin-r-t", "card--content__input--dropdown", { opened: this.state.showOutputDetails === idx })}>
+                                        <div className={classNames("margin-r-t", "card--content__input--dropdown", "card--content__flex_between", { opened: this.state.showOutputDetails === idx })}>
                                             <DropdownIcon />
                                         </div>
                                         <Bech32Address
@@ -282,22 +282,23 @@ class TransactionPayload extends AsyncComponent<TransactionPayloadProps, Transac
                                         <div className="card--value">
                                             {UnitsHelper.formatBest(output.amount)}
                                         </div>
-                                        {this.state.showOutputDetails === idx
-                                            ? (
-                                                <React.Fragment>
-                                                    <div className="card--label"> Address</div>
-                                                    <div className="card--value">
-                                                        <Bech32Address
-                                                            network={this.props.network}
-                                                            history={this.props.history}
-                                                            addressDetails={output.address}
-                                                            advancedMode={true}
-                                                            hideLabel={true}
-                                                            truncateAddress={false}
-                                                        />
-                                                    </div>
-                                                </React.Fragment>) : ""}
                                     </div>
+
+                                    {this.state.showOutputDetails === idx
+                                        ? (
+                                            <React.Fragment>
+                                                <div className="card--label"> Address</div>
+                                                <div className="card--value">
+                                                    <Bech32Address
+                                                        network={this.props.network}
+                                                        history={this.props.history}
+                                                        addressDetails={output.address}
+                                                        advancedMode={true}
+                                                        hideLabel={true}
+                                                        truncateAddress={false}
+                                                    />
+                                                </div>
+                                            </React.Fragment>) : ""}
                                 </React.Fragment>
                             ))}
                         </div>
