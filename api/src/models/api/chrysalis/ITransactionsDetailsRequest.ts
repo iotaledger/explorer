17a5f4d4931d93cf10104f38c328a2a9a97558da
rubalv96 <36295331+rubalv96@users.noreply.github.com>
2021-12-17 16:03:04 +0100

    Feat: Add support for address transaction history & global scope DID search (#78)
    * Feat: Add new API call (transaction history of an address)
    
    * Cleanup
    
    * Fix import
    
    * Feat: Add description to transactions response state field
    
    * Fix: Solved incompatible call methods to create instances of classes wich extends from external iota.js dependencies
    
    * Lint

---
+++ api/src/models/api/chrysalis/ITransactionsDetailsRequest.ts
@@ -0,0 +1,11 @@
+export interface ITransactionsDetailsRequest {
+    /**
+     * The network to search on.
+     */
+    network: string;
+
+    /**
+     * The address to get the details for.
+     */
+    address: string;
+}
