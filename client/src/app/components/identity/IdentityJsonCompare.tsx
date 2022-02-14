f54573b2e053d304ae960356b4d4c738f1c3bb80
evavirseda <72078237+evavirseda@users.noreply.github.com>
2022-02-14 13:19:09 +0100

    Update toggle button (#168)

--- client/src/app/components/identity/IdentityJsonCompare.tsx
+++ client/src/app/components/identity/IdentityJsonCompare.tsx
@@ -70,7 +70,7 @@ class IdentityJsonCompare extends Component<IdentityJsonCompareProps, IdentityJs
                                     "toggle-button-active": this.state.toggleState === "doc"
                                 })}
                             >
-                                Raw
+                                Doc
                             </div>
                             <div className="toggle-separator" />
                             <div
@@ -78,7 +78,7 @@ class IdentityJsonCompare extends Component<IdentityJsonCompareProps, IdentityJs
                                     "toggle-button-active": this.state.toggleState === "msg"
                                 })}
                             >
-                                Normal
+                                Msg
                             </div>
                         </div>
                         {/* --------- Download Icon --------- */}
