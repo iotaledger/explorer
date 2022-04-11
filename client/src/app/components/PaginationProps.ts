4b58bee9d6e2f772388d9a2ecd45c90a71db842a
Branko Bosnic <52735957+brancoder@users.noreply.github.com>
2022-04-11 16:44:49 +0200

    Fix: Refine pagination (#182)
    * Refined pagination for large page numbers
    
    * fix: syntax and typo
    
    * fix: Remove hardcoded html code for three dots

--- client/src/app/components/PaginationProps.ts
+++ client/src/app/components/PaginationProps.ts
@@ -5,24 +5,29 @@ export interface PaginationProps {
      */
     totalCount: number;
 
-     /**
-      * The number of current page.
-      */
+    /**
+     * The number of current page.
+     */
     currentPage: number;
 
-     /**
-      * The total number of sibling pages.
-      */
+    /**
+     * The total number of sibling pages.
+     */
     siblingsCount: number;
 
-     /**
-      * Number of results per page.
-      */
+    /**
+     * Number of results per page.
+     */
     pageSize: number;
 
-     /**
-      * The optional additional CSS classes.
-      */
+    /**
+     * Define limit of remaining pages above which the extra page range will be shown.
+     */
+    extraPageRangeLimit?: number;
+
+    /**
+     * The optional additional CSS classes.
+     */
     classNames?: string;
 
     /**
