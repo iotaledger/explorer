dcbec164de51a044faf77739c743237742be3e2e
Mario Sarcevic <mario.sarcevic@iota.org>
2022-05-25 11:34:04 +0200

    feat: Adjust nft routes so they don't clash

--- client/src/services/stardust/stardustApiClient.ts
+++ client/src/services/stardust/stardustApiClient.ts
@@ -97,7 +97,7 @@ export class StardustApiClient extends ApiClient {
      */
      public async nftDetails(request: INftDetailsRequest): Promise<INftDetailsResponse> {
         return this.callApi<unknown, INftDetailsResponse>(
-            `stardust/nfts/${request.network}/${request.nftId}`,
+            `stardust/nft/${request.network}/${request.nftId}`,
             "get"
         );
     }
