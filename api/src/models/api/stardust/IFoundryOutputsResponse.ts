3e353dc14e196156a849d4e22e51cb7a4a391480
Mario Sarcevic <mario.sarcevic@iota.org>
2022-05-30 14:37:12 +0200

    fix: Remove unused file

--- api/src/models/api/stardust/IFoundryOutputsResponse.ts
+++
@@ -1,9 +0,0 @@
-import { IOutputsResponse } from "@iota/iota.js-stardust";
-import { IResponse } from "../IResponse";
-
-export interface IFoundryOutputsResponse extends IResponse {
-    /**
-     * The output ids response.
-     */
-    foundryOutputsResponse?: IOutputsResponse;
-}
