b9cfb4ac05a5e05a2abca0a7027940a3ff8f91bc
Mario Sarcevic <mario.sarcevic@iota.org>
2022-06-28 09:44:00 +0200

    feat: Do not display headers in output details loading failed

--- client/src/app/routes/stardust/OutputPage.tsx
+++ client/src/app/routes/stardust/OutputPage.tsx
@@ -26,19 +26,18 @@ const OutputPage: React.FC<RouteComponentProps<OutputPageProps>> = (
     const { blockId, transactionId, outputIndex, isSpent, milestoneIndexSpent, milestoneTimestampSpent,
         transactionIdSpent, milestoneIndexBooked, milestoneTimestampBooked } = outputDetails?.metadata ?? {};
 
-    return (
-        <div className="output-page">
-            <div className="wrapper">
-                <div className="inner">
-                    <div className="output-page--header">
-                        <div className="row middle">
-                            <h1>
-                                Output
-                            </h1>
+        return (outputDetails &&
+            <div className="output-page">
+                <div className="wrapper">
+                    <div className="inner">
+                        <div className="output-page--header">
+                            <div className="row middle">
+                                <h1>
+                                    Output
+                                </h1>
+                            </div>
                         </div>
-                    </div>
-                    <div className="section">
-                        {outputDetails && (
+                        <div className="section">
                             <div className="card output">
                                 <Output
                                     network={network}
@@ -49,136 +48,134 @@ const OutputPage: React.FC<RouteComponentProps<OutputPageProps>> = (
                                     isPreExpanded={true}
                                 />
                             </div>
-                        )}
 
-                        <div className="section--header row row--tablet-responsive middle space-between">
-                            <div className="row middle">
-                                <h2>Metadata</h2>
+                            <div className="section--header row row--tablet-responsive middle space-between">
+                                <div className="row middle">
+                                    <h2>Metadata</h2>
+                                </div>
                             </div>
+
+                            {blockId && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Block ID
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {blockId}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {transactionId && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Transaction ID
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {transactionId}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {outputIndex !== undefined && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Output index
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {outputIndex}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {isSpent !== undefined && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Is spent ?
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {isSpent.toString()}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {milestoneIndexSpent && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Spent at milestone
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {milestoneIndexSpent}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {milestoneTimestampSpent && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Spent at milestone timestamp
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {DateHelper.formatShort(milestoneTimestampSpent * 1000)}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {transactionIdSpent && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Spent in transaction with ID
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {transactionIdSpent}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {milestoneIndexBooked && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Booked at milestone
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {milestoneIndexBooked}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
+                            {milestoneTimestampBooked && (
+                                <div className="section--data">
+                                    <div className="label">
+                                        Booked on
+                                    </div>
+                                    <div className="value code row middle">
+                                        <span className="margin-r-t">
+                                            {DateHelper.formatShort(milestoneTimestampBooked * 1000)}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
                         </div>
 
-                        {blockId && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Block ID
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {blockId}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {transactionId && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Transaction ID
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {transactionId}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {outputIndex !== undefined && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Output index
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {outputIndex}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {isSpent !== undefined && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Is spent ?
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {isSpent.toString()}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {milestoneIndexSpent && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Spent at milestone
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {milestoneIndexSpent}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {milestoneTimestampSpent && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Spent at milestone timestamp
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {DateHelper.formatShort(milestoneTimestampSpent * 1000)}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {transactionIdSpent && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Spent in transaction with ID
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {transactionIdSpent}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {milestoneIndexBooked && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Booked at milestone
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {milestoneIndexBooked}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
-
-                        {milestoneTimestampBooked && (
-                            <div className="section--data">
-                                <div className="label">
-                                    Booked on
-                                </div>
-                                <div className="value code row middle">
-                                    <span className="margin-r-t">
-                                        {DateHelper.formatShort(milestoneTimestampBooked * 1000)}
-                                    </span>
-                                </div>
-                            </div>
-                        )}
                     </div>
-
                 </div>
-            </div>
-        </div>
-    );
+            </div>) ?? null;
 };
 
 
