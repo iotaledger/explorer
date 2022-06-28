208e63bcceede0bd9cf5fc944b4554c7119bca19
Mario Sarcevic <mario.sarcevic@iota.org>
2022-06-28 09:14:42 +0200

    feat: Add dedicated OutputPage Fix bug in api stardustTangleHelper

--- client/src/app/routes/SearchState.ts
+++ client/src/app/routes/SearchState.ts
@@ -30,4 +30,9 @@ export interface SearchState {
      * Redirect to another page.
      */
     redirect: string;
+
+    /**
+     * State to pass in the redirect
+     */
+    redirectState?: unknown;
 }
---
+++ client/src/app/routes/stardust/OutputPageProps.ts
@@ -0,0 +1,11 @@
+export default interface OutputPageProps {
+    /**
+     * The network to lookup.
+     */
+    network: string;
+
+    /**
+     * The output id to lookup.
+     */
+    outputId: string;
+}
