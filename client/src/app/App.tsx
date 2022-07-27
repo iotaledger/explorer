e9d7984ec6943b0b19b11bb229a55cd679476ac2
Mario Sarcevic <mario.sarcevic@iota.org>
2022-07-27 16:43:10 +0200

    fix: Fix NftRegistryDetails route to prevent colision

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -332,7 +332,7 @@ class App extends Component<RouteComponentProps<AppRouteProps> & { config: IConf
                                                         )}
                                                     />
                                                     <Route
-                                                        path="/:network/nft/registry/:nftId"
+                                                        path="/:network/nft-registry/:nftId"
                                                         component={
                                                             (props: RouteComponentProps<NftRegistryDetailsProps>) => (
                                                                 <NftRegistryDetails {...props} />
