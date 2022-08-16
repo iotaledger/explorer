fd39f016fdf34b00e45dd0e157bd128293e4b32b
Mario Sarcevic <mario.sarcevic@iota.org>
2022-08-16 16:14:55 +0200

    fix: Decouple FeedInfo seconds since last milestone from milestone timestamp Interval just restarts from 0 everytime the milestone changes

--- client/src/app/components/chrysalis/Feeds.tsx
+++ client/src/app/components/chrysalis/Feeds.tsx
@@ -111,30 +111,22 @@ abstract class Feeds<P extends RouteComponentProps<{ network: string }>, S exten
      * @param newItems The updated items.
      */
     protected itemsUpdated(newItems: IFeedItem[]): void {
-        const isLatestMilesontFeedInfoEnabled = this._networkConfig &&
+        const isLatestMilesoneFeedInfoEnabled = this._networkConfig &&
             this._networkConfig.network !== LEGACY_MAINNET &&
             this._networkConfig.network !== CUSTOM;
 
-        if (isLatestMilesontFeedInfoEnabled && newItems) {
+        if (isLatestMilesoneFeedInfoEnabled && newItems) {
             const milestones = newItems.filter(i => i.payloadType === "MS");
             let newIndex;
-            let newTimestamp;
             for (const ms of milestones) {
                 const index: number | undefined = ms.properties?.index as number;
                 const currentIndex = this.state.latestMilestoneIndex;
                 if (index && currentIndex !== undefined && index > currentIndex) {
-                    const timestamp: number | undefined = ms.properties?.timestamp as number;
-                    if (timestamp) {
-                        newIndex = index;
-                        newTimestamp = timestamp;
-                    }
+                    newIndex = index;
                 }
             }
-            if (newIndex && newTimestamp) {
-                this.setState({
-                    latestMilestoneIndex: newIndex,
-                    latestMilestoneTimestamp: newTimestamp * 1000
-                });
+            if (newIndex) {
+                this.setState({ latestMilestoneIndex: newIndex });
             }
         }
     }
@@ -229,8 +221,6 @@ abstract class Feeds<P extends RouteComponentProps<{ network: string }>, S exten
                         ? `${confirmedRate.toFixed(2)}%` : "--",
                     latestMilestoneIndex: this.state.latestMilestoneIndex ? this.state.latestMilestoneIndex :
                         ips.latestMilestoneIndex,
-                    latestMilestoneTimestamp: this.state.latestMilestoneTimestamp ?
-                        this.state.latestMilestoneTimestamp : ips.latestMilestoneIndexTime,
                     // Increase values by +100 to add more area under the graph
                     itemsPerSecondHistory: (ips.itemsPerSecondHistory ?? []).map(v => v + 100)
                 });
