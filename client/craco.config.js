a84dbc288a254fe683016707f7c21289ee5c1181
Mario Sarcevic <mario.sarcevic@iota.org>
2022-08-30 10:07:33 +0200

    chore: Craco enable historyApiFallback

--- client/craco.config.js
+++ client/craco.config.js
@@ -35,6 +35,7 @@ module.exports = {
                 );
                 return middlewares;
             },
+            historyApiFallback: true
         };
         return devServerConfig;
     },
