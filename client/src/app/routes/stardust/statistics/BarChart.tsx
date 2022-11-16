44d5cf7ba444630e5f1342046c87959a32b835a4
Mario Sarcevic <mario.sarcevic@iota.org>
2022-11-16 11:07:13 +0100

    feat: Implement basic BarChart component using d3.js  + Enable StatisticsPage route  + Remove chart.js dependencies

---
+++ client/src/app/routes/stardust/statistics/BarChart.tsx
@@ -0,0 +1,67 @@
+import { max } from "d3-array";
+import { axisBottom, axisLeft } from "d3-axis";
+import { scaleBand, scaleLinear } from "d3-scale";
+import { select } from "d3-selection";
+import moment from "moment";
+import React, { useLayoutEffect, useRef } from "react";
+import "./BarChart.scss";
+
+interface BarChartProps {
+    width: number;
+    height: number;
+    data: { time: string; n: number }[];
+}
+
+const BarChart: React.FC<BarChartProps> = ({ height, width, data }) => {
+    const theSvg = useRef<SVGSVGElement>(null);
+
+    useLayoutEffect(() => {
+        const MARGIN = { top: 30, right: 20, bottom: 30, left: 50 };
+        const INNER_WIDTH = width - MARGIN.left - MARGIN.right;
+        const INNER_HEIGHT = height - MARGIN.top - MARGIN.bottom;
+
+        const x = scaleBand().domain(data.map(d => d.time))
+            .range([0, INNER_WIDTH])
+            .paddingInner(0.1);
+
+        const dataMaxN = max(data, d => d.n) ?? 1;
+        const y = scaleLinear().domain([0, dataMaxN])
+            .range([INNER_HEIGHT, 0]);
+
+        const svg = select(theSvg.current)
+            .attr("width", INNER_WIDTH + MARGIN.left + MARGIN.right)
+            .attr("height", INNER_HEIGHT + MARGIN.top + MARGIN.bottom)
+            .append("g")
+            .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`);
+
+        const yAxisGrid = axisLeft(y.nice()).tickSize(-INNER_WIDTH).tickPadding(10);
+        svg.append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxisGrid);
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => x(d.time) ?? 0)
+            .attr("width", x.bandwidth())
+            .attr("y", d => y(d.n))
+            .attr("height", d => INNER_HEIGHT - y(d.n))
+            .attr("fill", "#14cabf");
+
+        const xAxis = axisBottom(x).tickPadding(10).tickFormat(time => moment(time).format("DD MMM"));
+        svg.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", `translate(0, ${INNER_HEIGHT})`)
+            .call(xAxis);
+    }, [width, height, data]);
+
+    return (
+        <div className="bar-chart--wrapper">
+            <svg className="hook" ref={theSvg} />
+        </div>
+    );
+};
+
+export default BarChart;
---
+++ client/src/app/routes/stardust/statistics/StatisticsPage.tsx
@@ -0,0 +1,61 @@
+import moment from "moment";
+import React from "react";
+import { response } from "../../../../helpers/stardust/mockDailyTransactions";
+import BarChart from "./BarChart";
+import "./StatisticsPage.scss";
+
+const StatisticsPage: React.FC<null> = () => {
+    const seenDays: number[] = [];
+    const data = response.reduce((acc: { time: string; n: number }[], next) => {
+        const m = moment(next.time);
+        const dOY = m.dayOfYear();
+        if (!seenDays.includes(dOY)) {
+            acc.push(next);
+            seenDays.push(dOY);
+        }
+        return acc;
+    }, []);
+
+    return (
+        <div className="statistics-page">
+            <div className="wrapper">
+                <div className="inner">
+                    <div className="statistics-page--header">
+                        <div className="row middle">
+                            <h1>
+                                Statistics
+                            </h1>
+                        </div>
+                    </div>
+                    <div className="statistics-page--content">
+                        <div className="section">
+                            <div className="section--header">
+                                <h2>Random</h2>
+                            </div>
+                            <BarChart
+                                width={1172}
+                                height={550}
+                                data={data}
+                            />
+                            <div className="row middle space-between">
+                                <BarChart
+                                    width={566}
+                                    height={550}
+                                    data={data}
+                                />
+                                <BarChart
+                                    width={566}
+                                    height={550}
+                                    data={data}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StatisticsPage;
+
