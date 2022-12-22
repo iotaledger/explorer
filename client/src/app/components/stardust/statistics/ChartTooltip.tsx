da352ec91c782bbe267b9eea51ff62a9f3405e0f
Branko Bosnic <brankobosnic1@gmail.com>
2022-12-22 14:01:08 +0100

    fix: chart tooltip offscreen when its too large to fit on either left or right side of the cursor pointer and edge of the screen (mobile)

--- client/src/app/components/stardust/statistics/ChartTooltip.tsx
+++ client/src/app/components/stardust/statistics/ChartTooltip.tsx
@@ -22,11 +22,16 @@ const ChartTooltip: React.FC<ChartTooltipProps> = ({ tooltipRef }) => {
 
     const getX = () => {
         let x = position.x + 8;
-        const remainingWidth = window.innerWidth - (position.x + 20);
+        const tooltipWidth = tooltipRef.current?.offsetWidth ?? 0;
+        const remainingWidthRight = window.innerWidth - (position.x + 20);
+        const remainingWidthLeft = position.x;
 
-        if (tooltipRef.current && remainingWidth <= tooltipRef.current.offsetWidth) {
-            x = position.x - tooltipRef.current.offsetWidth;
+        if (remainingWidthRight <= tooltipWidth && remainingWidthLeft <= tooltipWidth) {
+            x = position.x - (tooltipWidth / 2);
+        } else if (remainingWidthRight <= tooltipWidth) {
+            x = position.x - tooltipWidth;
         }
+
         return x;
     };
 
