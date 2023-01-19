030a6055c0ed1d51b6079605e35fd705f7407b35
Mario Sarcevic <mario.sarcevic@iota.org>
2023-01-19 12:55:30 +0100

    fix: Remove unused import

--- client/src/app/components/stardust/section/referenced-blocks/ReferencedBlocksSection.tsx
+++ client/src/app/components/stardust/section/referenced-blocks/ReferencedBlocksSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Pagination from "../../../Pagination";
 import Spinner from "../../../Spinner";
 import ReferencedBlocksSectionRow from "./ReferencedBlocksSectionRow";
