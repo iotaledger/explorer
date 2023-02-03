d42e36e9c006e15fa9755679cc9b9868d31500e1
Branko Bosnic <brankobosnic1@gmail.com>
2023-02-03 12:11:34 +0100

    fix: dynamic link based on address type in Address.tsx

--- client/src/app/components/stardust/Address.tsx
+++ client/src/app/components/stardust/Address.tsx
@@ -1,3 +1,4 @@
+import { ED25519_ADDRESS_TYPE, NFT_ADDRESS_TYPE } from "@iota/iota.js-stardust";
 import React, { Component, ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { Bech32AddressHelper } from "../../../helpers/stardust/bech32AddressHelper";
@@ -25,12 +26,15 @@ class Address extends Component<AddressProps> {
      * @returns The node to render.
      */
     public render(): ReactNode {
-        const bech32 = Bech32AddressHelper.buildAddress(
+        const address = Bech32AddressHelper.buildAddress(
             this.context.bech32Hrp,
             this.props.address
-        ).bech32;
+        );
+        const bech32Short = `${address.bech32.slice(0, 12)}....${address.bech32.slice(-12)}`;
 
-        const bech32Short = `${bech32.slice(0, 12)}....${bech32.slice(-12)}`;
+        const route = address.type === ED25519_ADDRESS_TYPE ? "addr" :
+                (address.type === NFT_ADDRESS_TYPE ? "nft" : "alias");
+        const link = `/${this.context.name}/${route}/${address.bech32}`;
         return (
             <div className="address-type">
                 <div className="card--label">
@@ -38,12 +42,12 @@ class Address extends Component<AddressProps> {
                 </div>
                 <div className="card--value row middle">
                     <Link
-                        to={`/${this.context.name}/addr/${bech32}`}
+                        to={link}
                         className="margin-r-t"
                     >
                         {bech32Short}
                     </Link>
-                    <CopyButton copy={bech32} />
+                    <CopyButton copy={address.bech32} />
                 </div>
             </div>
         );
