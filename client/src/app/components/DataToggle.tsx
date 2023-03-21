74a3243ce1d69eb870c632dfcfda2eee021289cd
Branko Bosnic <brankobosnic1@gmail.com>
2023-03-21 13:23:32 +0100

    fix: refactor DataToggle to functional component

--- client/src/app/components/DataToggle.tsx
+++ client/src/app/components/DataToggle.tsx
@@ -1,10 +1,10 @@
+import { HexEncodedString } from "@iota/iota.js-stardust";
 import { Converter } from "@iota/util.js-stardust";
 import classNames from "classnames";
-import React, { Component, ReactNode } from "react";
+import React, { useEffect, useState } from "react";
 import { TextHelper } from "../../helpers/textHelper";
 import "./DataToggle.scss";
 import { DataToggleProps } from "./DataToggleProps";
-import { DataToggleState } from "./DataToggleState";
 import JsonViewer from "./JsonViewer";
 
 interface DataToggleOption {
@@ -19,100 +19,71 @@ interface DataToggleOption {
     content?: string;
 }
 
-/**
- * Component which will display a section with different contents able to be navigate through tabs.
- */
-class DataToggle extends Component<DataToggleProps, DataToggleState> {
-    /**
-     * Create a new instance of DataToggle.
-     * @param props The props.
-     */
-    constructor(props: DataToggleProps) {
-        super(props);
-        this.state = {
-            activeTab: 0,
-            hexView: this.props.sourceData
-        };
-    }
+const DataToggle: React.FC<DataToggleProps> = (
+    { sourceData, link, withSpacedHex }
+) => {
+    const [options, setOptions] = useState<DataToggleOption[]>([]);
+    const [isJson, setIsJson] = useState<boolean>(false);
+    const [activeContent, setActiveContent] = useState<string>();
+    const [activeTab, setActiveTab] = useState<number>(0);
 
-    public componentDidMount(): void {
-        this.populateFormats();
-    }
+    useEffect(() => {
+        let utf8View: string | undefined;
+        let jsonView: string | undefined;
+        let hexView: HexEncodedString | undefined;
+        const dtOptions: DataToggleOption[] = [];
+        const hasSpacesBetweenBytes = withSpacedHex ?? false;
 
-    /**
-     * Render the component.
-     * @returns The node to render.
-     */
-    public render(): ReactNode {
-        const { link } = this.props;
-        const { activeTab, hexView, utf8View, jsonView } = this.state;
-
-        const options: DataToggleOption[] = [];
-        if (jsonView) {
-            options.push({ label: "JSON", content: jsonView });
-        } else if (utf8View) {
-            options.push({ label: "Text", content: utf8View });
-        }
-        options.push({ label: "HEX", content: hexView });
-
-        const activeOption: DataToggleOption = options[activeTab];
-        const activeContent = activeOption.content;
-
-        const isJson = activeOption.label === "JSON";
-
-        return (
-            <div className="data-toggle">
-                {link ?
-                    <a className="data-toggle--content" href={link}>{activeContent}</a> :
-                    (isJson ?
-                        <div className="data-toggle--content"><JsonViewer json={activeContent} /></div> :
-                        <div className="data-toggle--content">{activeContent}</div>)}
-                <div className="data-toggle--tabs">
-                    {options.map((option, index) => (
-                        option.content ? (
-                            <div
-                                key={option.label}
-                                className={classNames(
-                                    "data-toggle--tab", { "data-toggle--tab__active": activeTab === index }
-                                )}
-                                onClick={() => this.setState({ activeTab: index })}
-                            >
-                                {option.label}
-                            </div>) : null
-                    ))}
-                </div>
-            </div>
-        );
-    }
-
-    /**
-     * Converts hex data to UTF-8 and Json, if possible.
-     */
-    private populateFormats() {
-        let utf8View;
-        let jsonView;
-        let hexView = this.state.hexView;
-        const checkValidUtf8 = true;
-
-        const isSpacesBetweenBytes = this.props.withSpacedHex ?? false;
-
-        if (!checkValidUtf8 || (checkValidUtf8 && TextHelper.isUTF8(Converter.hexToBytes(hexView)))) {
-            utf8View = Converter.hexToUtf8(hexView);
-        }
-
-        if (isSpacesBetweenBytes) {
-            const canBeSpacedMatch = hexView.match(/.{1,2}/g);
-            hexView = canBeSpacedMatch ? canBeSpacedMatch.join(" ") : hexView;
-        }
-
-        try {
-            if (utf8View) {
+        if (TextHelper.isUTF8(Converter.hexToBytes(sourceData))) {
+            utf8View = Converter.hexToUtf8(sourceData);
+            try {
                 jsonView = JSON.stringify(JSON.parse(utf8View), undefined, "  ");
-            }
-        } catch { }
+            } catch { }
+        }
 
-        this.setState({ hexView, utf8View, jsonView });
-    }
-}
+        if (hasSpacesBetweenBytes) {
+            const canBeSpacedMatch = sourceData.match(/.{1,2}/g);
+            hexView = canBeSpacedMatch ? canBeSpacedMatch.join(" ") : sourceData;
+        }
+
+        if (jsonView) {
+            dtOptions.push({ label: "JSON", content: jsonView });
+        } else if (utf8View) {
+            dtOptions.push({ label: "Text", content: utf8View });
+        }
+        dtOptions.push({ label: "HEX", content: hexView });
+        setOptions(dtOptions);
+    }, [sourceData]);
+
+    useEffect(() => {
+        const activeOption = options[activeTab];
+        setIsJson(activeOption.label === "JSON");
+        setActiveContent(activeOption.content);
+    }, [activeTab]);
+
+    return (
+        <div className="data-toggle">
+            {link ?
+                <a className="data-toggle--content" href={link}>{activeContent}</a> :
+                (isJson ?
+                    <div className="data-toggle--content"><JsonViewer json={activeContent} /></div> :
+                    <div className="data-toggle--content">{activeContent}</div>)}
+            <div className="data-toggle--tabs">
+                {options.map((option, index) => (
+                    option.content ? (
+                        <div
+                            key={option.label}
+                            className={classNames(
+                                "data-toggle--tab", { "data-toggle--tab__active": activeTab === index }
+                            )}
+                            onClick={() => setActiveTab(index)}
+                        >
+                            {option.label}
+                        </div>) : null
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default DataToggle;
