bc1770225cf99ecb3991d4a4f0950d180c2914e3
Mario Sarcevic <mario.sarcevic@iota.org>
2023-03-21 11:01:02 +0100

    feat(logging): Prevent space before version if env variable is not present

--- api/src/logger.ts
+++ api/src/logger.ts
@@ -3,7 +3,7 @@ import { createLogger, format, transports } from "winston";
 
 // error < warn < info < verbose < debug
 const logLevel: string = process.env.LOG_LEVEL ?? "info";
-const version = process.env.npm_package_version ?? "";
+const version = process.env.npm_package_version ? ` ${process.env.npm_package_version}` : "";
 
 const { combine, timestamp: timestampFunc, label: labelFunc, printf } = format;
 const theFormat = combine(
@@ -17,7 +17,7 @@ const theFormat = combine(
 const logger = createLogger({
     level: logLevel,
     format: format.json(),
-    defaultMeta: { service: `Explorer ${version}` },
+    defaultMeta: { service: `Explorer${version}` },
     transports: [
         new transports.Console({
             level: logLevel,
