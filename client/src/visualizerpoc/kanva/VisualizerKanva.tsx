6d649cad74c7eb804d3c6f0c4c67103e237a2feb
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2023-07-03 19:19:01 +0300

    Shift graph when run.

--- client/src/visualizerpoc/kanva/VisualizerKanva.tsx
+++ client/src/visualizerpoc/kanva/VisualizerKanva.tsx
@@ -6,7 +6,7 @@ import { RouteComponentProps } from "react-router-dom";
 import { VisualizerRouteProps } from "../../app/routes/VisualizerRouteProps";
 import { IFeedBlockData } from "../../models/api/stardust/feed/IFeedBlockData";
 import { useUpdateListener } from "../common/useUpdateListener";
-import { placeNodeCallback } from "../vivagraph-layout/layout";
+import { placeNodeCallback, THRESHOLD_PX } from "../vivagraph-layout/layout";
 import { LIMIT_NODES } from "./constants";
 import { useDrag } from "./useDrag";
 import { useStoreIds } from "./useStoreIds";
@@ -36,9 +36,13 @@ export const VisualizerKanva: React.FC<RouteComponentProps<VisualizerRouteProps>
     const stageRef = useRef<Konva.Stage>(null);
     const lastNodePositionRef = useRef<number>(0);
     const { handleWheel } = useZoom();
-    const { handleMouseMove, handleMouseUp, handleMouseDown } = useDrag();
+    const {
+        handleMouseMove,
+        handleMouseUp,
+        handleMouseDown,
+        shiftGraphRight
+    } = useDrag(stageRef, layerRef, lastNodePositionRef);
     const { removeFirstNodeFromLayer, storeAddBlock, getNumberOfNodes } = useStoreIds();
-
 
     const onNewBlock = (block: IFeedBlockData) => {
         if (layerRef.current) {
@@ -65,6 +69,8 @@ export const VisualizerKanva: React.FC<RouteComponentProps<VisualizerRouteProps>
             });
             layerRef.current.add(circle);
 
+            shiftGraphRight();
+
             // now we need to to refresh the layer manually
             layerRef.current.batchDraw();
         }
@@ -72,27 +78,11 @@ export const VisualizerKanva: React.FC<RouteComponentProps<VisualizerRouteProps>
 
     const { nodes } = useUpdateListener(network, onNewBlock);
 
-    useEffect(() => {
-        if (stageRef.current) {
-            // Set the initial scale of the stage
-            stageRef.current.scale({ x: 0.1, y: 0.1 }); // Set the scale as needed
-
-            // Set the initial position of the stage
-            // const canvasHeight =
-            // window.sc = stageRef.current;
-            const viewportHeight = stageRef.current.height(); // The height of the viewport
-            const nodeRangeCenter = 400; // The center of the Y range of the nodes (0 + 800) / 2
-            const initialY = ((viewportHeight - nodeRangeCenter) / 2) + (nodeRangeCenter / 2);
-            console.log("--- initialY", viewportHeight, initialY);
-
-            stageRef.current.position({ x: 0, y: initialY });
-        }
-    }, []);
-
 
     return (
         <Stage
-            width={window.innerWidth - 50} height={window.innerHeight}
+            width={window.innerWidth - 50}
+            height={window.innerHeight}
             onWheel={handleWheel}
             draggable
             onMouseDown={handleMouseDown}
--- client/src/visualizerpoc/kanva/useDrag.ts
+++ client/src/visualizerpoc/kanva/useDrag.ts
@@ -1,13 +1,14 @@
 import Konva from "konva";
-import { useState } from "react";
+import { useEffect, useState, RefObject } from "react";
+import { THRESHOLD_PX } from "../vivagraph-layout/layout";
 
-export const useDrag = () => {
+export const useDrag = (stageRef: RefObject<Konva.Stage>, layerRef: RefObject<Konva.Layer>, lastNodePositionRef: RefObject<number>) => {
     const [isDragging, setIsDragging] = useState(false);
     const [pos, setPos] = useState({ x: 0, y: 0 });
 
     const handleMouseDown = (event: Konva.KonvaEventObject<MouseEvent>) => {
         const stage = event.target.getStage();
-        if (stage) {
+        if (stage?.getPointerPosition()) {
             // @ts-expect-error
             setPos(stage.getPointerPosition());
         }
@@ -38,10 +39,60 @@ export const useDrag = () => {
         }
     };
 
+    const getYCenterCoordinate = () => {
+        if (!stageRef.current) {
+            return 0;
+        }
+        // Set the initial position of the stage
+        const viewportHeight = stageRef.current.height(); // The height of the viewport
+        const nodeRangeCenter = -400; // The center of the Y range of the nodes (0 + 800) / 2
+        return ((viewportHeight - nodeRangeCenter) / 2) + (nodeRangeCenter / 2);
+    };
+
+    const getXCenterCoordinate = () => {
+        if (!stageRef.current) {
+            return 0;
+        }
+        // Set the initial position of the stage
+        const viewportWidth = stageRef.current.width(); // The width of the viewport
+        const nodeRangeCenter = -400; // The center of the Y range of the nodes (0 + 800) / 2
+        return ((viewportWidth - nodeRangeCenter) / 2) + (nodeRangeCenter / 2);
+    };
+
+    const shiftGraphRight = () => {
+        if (layerRef.current && lastNodePositionRef.current) {
+            const newPosition = -(lastNodePositionRef.current * THRESHOLD_PX);
+            layerRef.current.x(newPosition);
+
+
+            // Create and start the animation
+            // const tween = new Konva.Tween({
+            //     node: layerRef.current,
+            //     duration: 16, // The duration of the animation in seconds
+            //     x: newPosition
+            // });
+            // tween.play();
+            layerRef.current.batchDraw();
+        }
+    };
+
+    useEffect(() => {
+        if (stageRef.current) {
+            // Set the initial scale of the stage
+            stageRef.current.scale({ x: 0.1, y: 0.1 }); // Set the scale as needed
+
+            const initialY = getYCenterCoordinate();
+            const initialX = getXCenterCoordinate();
+
+            stageRef.current.position({ x: initialX, y: initialY });
+        }
+    }, []);
+
 
     return {
         handleMouseDown,
         handleMouseUp,
-        handleMouseMove
+        handleMouseMove,
+        shiftGraphRight
     };
 };
