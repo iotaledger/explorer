4c7644ab2a2056863bbc083b228d6f041ccb8133
Mario Sarcevic <mario.sarcevic@iota.org>
2023-11-23 15:55:09 +0100

    chore: Set port 3000 for vite preview

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(() => {
         server: {
             port: 3000
         },
+        preview: {
+            port: 3000
+        },
         build: {
             outDir: 'build',
         },
