bcd5642f03e64869c5984c5bfa37984278efc2bc
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2023-12-11 23:19:41 +0200

    feat: Improve Address Transaction History export (#853)
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- api/src/routes/stardust/transactionhistory/download/post.ts
+++ api/src/routes/stardust/transactionhistory/download/post.ts
@@ -1,13 +1,16 @@
 import JSZip from "jszip";
+import moment from "moment";
 import { ServiceFactory } from "../../../../factories/serviceFactory";
 import logger from "../../../../logger";
 import { IDataResponse } from "../../../../models/api/IDataResponse";
 import { ITransactionHistoryDownloadBody } from "../../../../models/api/stardust/chronicle/ITransactionHistoryDownloadBody";
 import { ITransactionHistoryRequest } from "../../../../models/api/stardust/chronicle/ITransactionHistoryRequest";
+import { IOutputDetailsResponse } from "../../../../models/api/stardust/IOutputDetailsResponse";
 import { IConfiguration } from "../../../../models/configuration/IConfiguration";
 import { STARDUST } from "../../../../models/db/protocolVersion";
 import { NetworkService } from "../../../../services/networkService";
 import { ChronicleService } from "../../../../services/stardust/chronicleService";
+import { StardustTangleHelper } from "../../../../utils/stardust/stardustTangleHelper";
 import { ValidationHelper } from "../../../../utils/validationHelper";
 
 /**
@@ -42,11 +45,47 @@ export async function post(
 
     const result = await chronicleService.transactionHistoryDownload(request.address, body.targetDate);
 
+    const outputDetails: IOutputDetailsResponse[] = await Promise.all(result.items.map(async item =>
+        StardustTangleHelper.outputDetails(networkConfig, item.outputId)
+    ));
+
+    const changeBalanceByTransactionId = new Map<string, { balance: number; timestamp: number }>();
+
+    for (const details of outputDetails) {
+        const metadata = details.output.metadata;
+        const amount = Number(details.output.output.amount);
+        const timestamp = metadata.isSpent ? metadata.milestoneTimestampSpent : metadata.milestoneTimestampBooked;
+        const transactionId = metadata.isSpent ? metadata.transactionIdSpent : metadata.transactionId;
+
+        const initTransactionInfo = {
+            balance: 0,
+            timestamp: 0
+        };
+        if (!changeBalanceByTransactionId.has(transactionId)) {
+            changeBalanceByTransactionId.set(transactionId, initTransactionInfo);
+        }
+        const prev = changeBalanceByTransactionId.get(transactionId);
+        prev.balance = metadata?.isSpent ? prev.balance - amount : prev.balance + amount;
+        prev.timestamp = Math.max(timestamp * 1000, prev.timestamp);
+        changeBalanceByTransactionId.set(transactionId, prev);
+    }
+
+    const headers = ["Timestamp", "TransactionId", "Balance changes"];
+
+    let csvContent = `${headers.join(",")}\n`;
+
+    for (const key of changeBalanceByTransactionId.keys()) {
+        const value = changeBalanceByTransactionId.get(key);
+        const row = [moment(value.timestamp).format("YYYY-MM-DD HH:mm:ss"), key, value.balance].join(",");
+        csvContent += `${row}\n`;
+    }
+
+
     const jsZip = new JSZip();
     let response: IDataResponse = null;
 
     try {
-        jsZip.file("history.json", JSON.stringify(result));
+        jsZip.file("history.csv", csvContent);
         const content = await jsZip.generateAsync({ type: "nodebuffer" });
 
         response = {
@@ -59,4 +98,5 @@ export async function post(
 
     return response;
 }
+
 
