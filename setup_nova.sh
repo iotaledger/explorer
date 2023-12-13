5603541d7ab646b942358284c06b8e88d480d8ed
Mario Sarcevic <mario.sarcevic@iota.org>
2023-12-13 13:34:21 +0100

    chore(local-nova-sdk): Add step in script to rename the nodejs&wasm bindings packages before install

--- setup_nova.sh
+++ setup_nova.sh
@@ -14,13 +14,17 @@ fi
 echo "Checking out nova-sdk commit $TARGET_COMMIT"
 git checkout "$TARGET_COMMIT"
 
-echo "Building nodejs bindings"
 cd "./bindings/nodejs"
+echo "Renaming nodejs sdk (sdk-nova)"
+sed -i '' '2s/.*/    \"name\": \"@iota\/sdk-nova\",/' package.json
+echo "Building nodejs bindings"
 yarn
 yarn build
 
-echo "Building wasm bindings"
 cd "../wasm"
+echo "Renaming wask sdk (sdk-wasm-nova)"
+sed -i '' '2s/.*/    \"name\": \"@iota\/sdk-wasm-nova\",/' package.json
+echo "Building wasm bindings"
 yarn
 yarn build
 
