c19c98d46b66319de326cdf0c107a6f49f04a621
Branko Bosnic <brankobosnic1@gmail.com>
2024-01-08 16:07:08 +0100

    fix: Add initial Block page structure

--- client/src/app/routes/nova/Block.tsx
+++ client/src/app/routes/nova/Block.tsx
@@ -1,8 +1,15 @@
 
 import React from "react";
 import { RouteComponentProps } from "react-router-dom";
+import mainHeaderMessage from "~assets/modals/stardust/block/main-header.json";
 import { useBlock } from "~helpers/nova/hooks/useBlock";
 import NotFound from "../../components/NotFound";
+import { BlockBodyType } from "@iota/sdk-wasm-nova/web";
+import Modal from "~/app/components/Modal";
+import Spinner from "~/app/components/Spinner";
+import TruncatedId from "~/app/components/stardust/TruncatedId";
+import { DateHelper } from "~/helpers/dateHelper";
+import MilestoneSignaturesSection from "~/app/components/stardust/block/payload/milestone/MilestoneSignaturesSection";
 
 export interface BlockProps {
     /**
@@ -20,13 +27,134 @@ const Block: React.FC<RouteComponentProps<BlockProps>> = (
     { history, match: { params: { network, blockId } } }
 ) => {
 
-    const [block, ,blockError] = useBlock(network, blockId);
+    const [block, isLoading, blockError] = useBlock(network, blockId);
+
+    let pageTitle = "Block";
+    switch (block?.body?.type) {
+        case BlockBodyType.Basic: {
+            pageTitle = `Basic ${pageTitle}`;
+            break;
+        }
+        case BlockBodyType.Validation: {
+            pageTitle = `Validation ${pageTitle}`;
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+    const blockContent = block ? (
+        <React.Fragment>
+            <div className="section--header row row--tablet-responsive middle space-between">
+                <div className="row middle">
+                    <h2>General</h2>
+                </div>
+            </div>
+            <div className="section--data">
+                <div className="label">
+                    Block ID
+                </div>
+                <div className="value code">
+                    <TruncatedId id={blockId} showCopyButton />
+                </div>
+            </div>
+            <div className="section--data">
+                <div className="label">
+                    Issuing Time
+                </div>
+                <div className="value code">
+                    {/* Convert nanoseconds to milliseconds */}
+                    {DateHelper.formatShort(Number(block.header.issuingTime) / 1000000)}
+                </div>
+            </div>
+            <div className="section--data">
+                <div className="label">
+                    Slot commitment
+                </div>
+                <div className="value code">
+                    <TruncatedId id={block.header.slotCommitmentId} />
+                </div>
+            </div>
+            <div className="section--data">
+                <div className="label">
+                    Issuer
+                </div>
+                <div className="value code">
+                    <TruncatedId id={block.header.issuerId} showCopyButton={true}/>
+                </div>
+            </div>
+
+            <div className="section--data row row--tablet-responsive">
+                {block.body?.strongParents && (
+                    <div className="truncate margin-b-s margin-r-m">
+                        <div className="label">
+                            Strong Parents
+                        </div>
+                        {block.body?.strongParents.map((parent, idx) => (
+                            <div
+                                key={idx}
+                                style={{ marginTop: "8px" }}
+                                className="value code link"
+                            >
+                                <TruncatedId
+                                    id={parent}
+                                    // link={isLinksDisabled ? undefined : `/${network}/block/${parent}`}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                )}
+                {block.body?.weakParents && (
+                    <div className="truncate">
+                        <div className="label">
+                            Weak Parents
+                        </div>
+                        {block.body?.weakParents.map((child, idx) => (
+                            <div
+                                key={idx}
+                                style={{ marginTop: "8px" }}
+                                className="value code link"
+                            >
+                                <TruncatedId
+                                    id={child}
+                                    // link={isLinksDisabled ? undefined : `/${network}/block/${child}`}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+            
+            {block.body.type === BlockBodyType.Basic && (
+                <div>
+                    {JSON.stringify(block.body)}
+                    <div className="section--data">
+                        <div className="label">
+                            Max burned mana
+                        </div>
+                        <div className="value code">
+                            {block.body.maxBurnedMana}
+                        </div>
+                    </div>
+                </div>
+            )}
+            <MilestoneSignaturesSection signatures={[block.signature]} />
+        </React.Fragment>
+    ) : null;
 
     if (blockError) {
         return (
             <div className="block">
                 <div className="wrapper">
                     <div className="inner">
+                        <div className="block--header">
+                            <div className="row middle">
+                                <h1>
+                                    {pageTitle}
+                                </h1>
+                                <Modal icon="info" data={mainHeaderMessage} />
+                            </div>
+                        </div>
                         <NotFound
                             searchTarget="block"
                             query={blockId}
@@ -41,7 +169,16 @@ const Block: React.FC<RouteComponentProps<BlockProps>> = (
         <div className="block">
             <div className="wrapper">
                 <div className="inner">
-                    <div className="section">{JSON.stringify(block)}</div>
+                    <div className="block--header">
+                        <div className="header--wrapper">
+                            <div className="row middle">
+                                <h1>{pageTitle}</h1>
+                                <Modal icon="info" data={mainHeaderMessage} />
+                                {isLoading && <Spinner />}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="section">{blockContent}</div>
                 </div>
             </div>
         </div>
