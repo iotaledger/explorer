004e059bd27c3a1878b17499d41087083b4cbb2f
JCNoguera <88061365+VmMad@users.noreply.github.com>
2024-01-11 10:43:02 +0100

    feat: improve radius based on throughput (#942)
    * feat: improve max tangle dynamic height
    
    * feat: add x position multiplier for different travel speeds
    
    * fix: remove extra semi

--- client/src/features/visualizer-threejs/constants.ts
+++ client/src/features/visualizer-threejs/constants.ts
@@ -2,7 +2,7 @@ import { Color } from "three";
 import { ThemeMode } from './enums';
 
 // steps
-export const STEP_Y_PX = 20;
+export const BLOCK_STEP_PX = 10;
 export const STEP_CAMERA_SHIFT_PX = 100;
 
 export const MAX_BLOCK_INSTANCES = 5000;
@@ -46,8 +46,8 @@ export const EMITTER_PADDING_RIGHT = 150
 export const VISUALIZER_SAFE_ZONE = 150
 
 // camera
-export const CAMERA_X_AXIS_MOVEMENT = 0.025
-export const CAMERA_Y_AXIS_MOVEMENT = 0.035
+export const CAMERA_X_AXIS_MOVEMENT = 0.005
+export const CAMERA_Y_AXIS_MOVEMENT = 0.065
 export const CAMERA_X_OFFSET = 0
 export const CAMERA_Y_OFFSET = 0.5
 
@@ -61,9 +61,6 @@ export const VISUALIZER_PADDINGS = {
   paddingTop: VISUALIZER_SAFE_ZONE,
 }
 
-// general
-export const MIN_BLOCKS_PER_SECOND = 50
-export const MAX_BLOCKS_PER_SECOND = 200
 
 // time
 export const MILLISECONDS_PER_SECOND = 1000
@@ -81,7 +78,22 @@ export const EMITTER_WIDTH = 30;
 export const EMITTER_HEIGHT = 250;
 export const EMITTER_DEPTH = 250;
 
+// conic emitter
+
+export const MIN_TANGLE_RADIUS = 100;
+export const MAX_TANGLE_RADIUS = 300;
+
+export const MIN_BLOCKS_PER_SECOND = 50;
+export const MAX_BLOCKS_PER_SECOND = 200;
+
+export const MIN_BLOCK_NEAR_RADIUS = 20;
+
+export const MAX_POINT_RETRIES = 10;
+export const MAX_PREV_POINTS = 20;
+
+export const EMITTER_X_POSITION_MULTIPLIER = 3;
+
 export const MAX_SINUSOIDAL_AMPLITUDE = 200;
 export const SINUSOIDAL_AMPLITUDE_ACCUMULATOR = 10;
-export const INITIAL_SINUSOIDAL_AMPLITUDE = 50;
+export const INITIAL_SINUSOIDAL_AMPLITUDE = 40;
 export const HALF_WAVE_PERIOD_SECONDS = 4;
