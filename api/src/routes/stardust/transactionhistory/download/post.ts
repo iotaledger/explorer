c5a5eb540e1fb2f7797262c8febc7f4b97d57c3e
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2024-01-30 12:15:57 +0200

    chore: calculate transactions.
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- api/src/routes/stardust/transactionhistory/download/post.ts
+++ api/src/routes/stardust/transactionhistory/download/post.ts
@@ -1,13 +1,22 @@
 // import JSZip from "jszip";
-// import moment from "moment";
-import { OutputResponse } from "@iota/sdk";
+import {
+    OutputResponse,
+    // INodeInfoBaseToken,
+    CommonOutput,
+} from "@iota/sdk";
+// import { Utils } from "@iota/sdk-wasm/web";
+// Utils.
+import moment from "moment";
 import { ServiceFactory } from "../../../../factories/serviceFactory";
 // import logger from "../../../../logger";
 import { IDataResponse } from "../../../../models/api/IDataResponse";
 import { ITransactionHistoryDownloadBody } from "../../../../models/api/stardust/chronicle/ITransactionHistoryDownloadBody";
 import { ITransactionHistoryRequest } from "../../../../models/api/stardust/chronicle/ITransactionHistoryRequest";
-import { ITransactionHistoryResponse } from "../../../../models/api/stardust/chronicle/ITransactionHistoryResponse";
-import { IOutputDetailsResponse } from "../../../../models/api/stardust/IOutputDetailsResponse";
+import {
+    // ITransactionHistoryResponse,
+    ITransactionHistoryItem,
+} from "../../../../models/api/stardust/chronicle/ITransactionHistoryResponse";
+// import { IOutputDetailsResponse } from "../../../../models/api/stardust/IOutputDetailsResponse";
 import { IConfiguration } from "../../../../models/configuration/IConfiguration";
 import { STARDUST } from "../../../../models/db/protocolVersion";
 import { NetworkService } from "../../../../services/networkService";
@@ -15,7 +24,18 @@ import { ChronicleService } from "../../../../services/stardust/chronicleService
 // import { StardustTangleHelper } from "../../../../utils/stardust/stardustTangleHelper";
 import { StardustApiService } from "../../../../services/stardust/stardustApiService";
 import { ValidationHelper } from "../../../../utils/validationHelper";
-export type OutputWithDetails = ITransactionHistoryResponse & { details: OutputResponse | null; amount?: string };
+export type OutputWithDetails = ITransactionHistoryItem & { details: OutputResponse | null; amount?: string };
+
+export interface ITransactionHistoryRecord {
+    isGenesisByDate: boolean;
+    isSpent: boolean;
+    transactionId: string;
+    timestamp: number;
+    dateFormatted: string;
+    balanceChange: number;
+    balanceChangeFormatted: string;
+    outputs: OutputWithDetails[];
+}
 
 /**
  * Download the transaction history from chronicle stardust.
@@ -78,12 +98,21 @@ export async function post(
         }),
     );
 
-    console.log('--- fulfilledOutputs', fulfilledOutputs);
+    fulfilledOutputs.sort((a, b) => {
+        // Ensure that entries with equal timestamp, but different isSpent,
+        // have the spending before the depositing
+        if (a.milestoneTimestamp === b.milestoneTimestamp && a.isSpent !== b.isSpent) {
+            return a.isSpent ? 1 : -1;
+        }
+        return 1;
+    });
+
+    const transactionIdToOutputs = groupOutputsByTransactionId(fulfilledOutputs);
+    const transactions = getTransactionHistoryRecords(transactionIdToOutputs);
+
+    console.log("--- transactions", transactions);
 
     // let transactionIdToOutputs = new Map<string, IOutputDetailsResponse[]>();
-
-
-
     // const outputDetails: IOutputDetailsResponse[] = await Promise.all(
     //     outputs.items.map(async (item) => apiService.outputDetails(item.outputId)),
     // );
@@ -138,3 +167,76 @@ export async function post(
     // return response;
     return null;
 }
+
+export const groupOutputsByTransactionId = (outputsWithDetails: OutputWithDetails[]) => {
+    const transactionIdToOutputs = new Map<string, OutputWithDetails[]>();
+    for (const output of outputsWithDetails) {
+        const detailsMetadata = output?.details?.metadata;
+        if (!detailsMetadata) {
+            // eslint-disable-next-line no-continue
+            continue;
+        }
+
+        const transactionId = output.isSpent ? detailsMetadata.transactionIdSpent : detailsMetadata.transactionId;
+
+        if (!transactionId) {
+            // eslint-disable-next-line no-continue
+            continue;
+        }
+
+        // if we don't have the transaction
+        const previousOutputs = transactionIdToOutputs.get(transactionId);
+        if (previousOutputs) {
+            transactionIdToOutputs.set(transactionId, [...previousOutputs, output]);
+        } else {
+            transactionIdToOutputs.set(transactionId, [output]);
+        }
+    }
+
+    return transactionIdToOutputs;
+};
+
+export const getTransactionHistoryRecords = (transactionIdToOutputs: Map<string, OutputWithDetails[]>): ITransactionHistoryRecord[] => {
+    const calculatedTransactions: ITransactionHistoryRecord[] = [];
+
+    for (const [transactionId, outputs] of transactionIdToOutputs.entries()) {
+        const lastOutputTime = Math.max(...outputs.map((t) => t.milestoneTimestamp));
+        const balanceChange = calculateBalanceChange(outputs);
+        const ago = moment(lastOutputTime * 1000).fromNow();
+
+        const isGenesisByDate = outputs.map((t) => t.milestoneTimestamp).includes(0);
+
+        const isSpent = balanceChange <= 0;
+
+        calculatedTransactions.push({
+            isGenesisByDate,
+            isSpent,
+            transactionId,
+            timestamp: lastOutputTime,
+            dateFormatted: `${moment(lastOutputTime * 1000).format("YYYY-MM-DD HH:mm:ss")} (${ago})`,
+            balanceChange,
+            balanceChangeFormatted: (isSpent ? "-" : "+") + Math.abs(balanceChange),
+            outputs,
+        });
+    }
+    return calculatedTransactions;
+};
+
+export const calculateBalanceChange = (outputs: OutputWithDetails[]) => {
+    // eslint-disable-next-line unicorn/no-array-reduce
+    return outputs.reduce((acc, output) => {
+        const outputFromDetails = output?.details?.output as CommonOutput;
+
+        if (!outputFromDetails?.amount) {
+            console.warn("Output details not found for:", output);
+            return acc;
+        }
+
+        let amount = Number(outputFromDetails.amount);
+        if (output.isSpent) {
+            // eslint-disable-next-line operator-assignment
+            amount = -1 * amount;
+        }
+        return acc + amount;
+    }, 0);
+};
