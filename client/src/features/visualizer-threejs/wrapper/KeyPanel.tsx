003aeef016e68afb1015e72c209ceb2e43c49a2e
Eugene P <panteleymonchuk@gmail.com>
2024-02-13 15:56:00 +0200

    enhancement: update legend for visualizer (#1112)
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>

--- client/src/features/visualizer-threejs/wrapper/KeyPanel.tsx
+++ client/src/features/visualizer-threejs/wrapper/KeyPanel.tsx
@@ -1,34 +1,63 @@
 import React from "react";
+import { BlockState } from "@iota/sdk-wasm-nova/web";
 
 import "./KeyPanel.scss";
 
-export const KeyPanel: React.FC = () => (
-    <div className="key-panel-container">
-        <div className="card key-panel">
-            <div className="key-panel-item">
-                <div className="key-marker vertex-state--pending" />
-                <div className="key-label">Pending</div>
-            </div>
-            <div className="key-panel-item">
-                <div className="key-marker vertex-state--included" />
-                <div className="key-label">Included</div>
-            </div>
-            <div className="key-panel-item">
-                <div className="key-marker vertex-state--referenced" />
-                <div className="key-label">Referenced</div>
-            </div>
-            <div className="key-panel-item">
-                <div className="key-marker vertex-state--conflicting" />
-                <div className="key-label">Conflicting</div>
-            </div>
-            <div className="key-panel-item">
-                <div className="key-marker vertex-state--milestone" />
-                <div className="key-label">Milestone</div>
-            </div>
-            <div className="key-panel-item">
-                <div className="key-marker vertex-state--search-result" />
-                <div className="key-label">Search result</div>
+export const KeyPanel: React.FC = () => {
+    const statuses: {
+        label: string;
+        state: BlockState;
+        color: string;
+    }[] = [
+        {
+            label: "Pending",
+            state: "pending",
+            color: "#A6C3FC",
+        },
+        {
+            label: "Accepted",
+            state: "accepted",
+            color: "#0101AB",
+        },
+        {
+            label: "Confirmed",
+            state: "confirmed",
+            color: "#0000DB",
+        },
+        {
+            label: "Finalized",
+            state: "finalized",
+            color: "#0101FF",
+        },
+        {
+            label: "Rejected",
+            state: "rejected",
+            color: "#252525",
+        },
+        {
+            label: "Failed",
+            state: "failed",
+            color: "#ff1d38",
+        },
+    ];
+
+    return (
+        <div className="key-panel-container">
+            <div className="card key-panel">
+                {statuses.map((s) => {
+                    return (
+                        <div className="key-panel-item" key={s.state}>
+                            <div
+                                className="key-marker"
+                                style={{
+                                    backgroundColor: s.color,
+                                }}
+                            />
+                            <div className="key-label">{s.label}</div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
-    </div>
-);
+    );
+};
