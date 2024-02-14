dae92f78ea6f097a488b43d32822d2d6cf1a5779
Bran <52735957+brancoder@users.noreply.github.com>
2024-02-14 11:02:11 +0100

    feat: add basic nova search (#1102)
    * feat: add basic block search
    
    * fix: add search for outputs and addresses
    
    * Refactor searchExecutor.ts to handle API response errors
    
    * fix: lint errors
    
    * fix: lint errors
    
    * fix: lint errors
    
    * fix: add slotIndex to searchQueryBuilder
    
    * doc: fix typo
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

---
+++ client/src/models/api/nova/ISearchResponse.ts
@@ -0,0 +1,60 @@
+import { Block, OutputResponse } from "@iota/sdk-wasm-nova/web";
+import { IResponse } from "./IResponse";
+import { IAddressDetails } from "./IAddressDetails";
+
+export interface ISearchResponse extends IResponse {
+    /**
+     * Block if it was found.
+     */
+    block?: Block;
+
+    /**
+     * Output if it was found (block will also be populated).
+     */
+    output?: OutputResponse;
+
+    /**
+     * Address details.
+     */
+    addressDetails?: IAddressDetails;
+
+    /**
+     * Transaction id if it was found.
+     */
+    transactionId?: string;
+
+    /**
+     * Account id if it was found.
+     */
+    accountId?: string;
+
+    /**
+     * Account details.
+     */
+    accountDetails?: OutputResponse;
+
+    /**
+     * Anchor id if it was found.
+     */
+    anchorId?: string;
+
+    /**
+     * Foundry id if it was found.
+     */
+    foundryId?: string;
+
+    /**
+     * Foundry details.
+     */
+    foundryDetails?: OutputResponse;
+
+    /**
+     * Nft id if it was found.
+     */
+    nftId?: string;
+
+    /**
+     * Nft details.
+     */
+    nftDetails?: OutputResponse;
+}
