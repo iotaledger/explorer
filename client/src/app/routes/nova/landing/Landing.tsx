c168157f576ff5082fdbc21b30c90cef318896e1
Mario <mario.sarcevic@iota.org>
2024-02-16 13:52:29 +0100

    Feat: Add Epoch section on Landing (#1135)
    * feat: Add more util functions in novaTimeUtils. Fix novaTimeUtils to be aliged with the go implementation (https://github.com/iotaledger/iota.go/blob/develop/timeprovider.go)
    
    * feat: Add nova Landing page. Add mock "epoch section" to landing (WiP)
    
    * feat: Add utils to get the registration slotIndex from an epoch index
    
    * feat: Wire up epoch stats in LandingEpochSection
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

---
+++ client/src/app/routes/nova/landing/Landing.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import LandingEpochSection from "~/app/components/nova/landing/LandingEpochSection";
+import { useNetworkConfig } from "~helpers/hooks/useNetworkConfig";
+import { LandingRouteProps } from "../../LandingRouteProps";
+import "./Landing.scss";
+
+const Landing: React.FC<RouteComponentProps<LandingRouteProps>> = ({
+    match: {
+        params: { network },
+    },
+}) => {
+    const [networkConfig] = useNetworkConfig(network);
+
+    return (
+        <div className="landing-nova">
+            <div className="header-wrapper">
+                <div className="inner">
+                    <div className="header">
+                        <div className="header--title">
+                            <h2>{networkConfig.isEnabled ? "Explore network" : ""}</h2>
+                            <div className="network-name">
+                                <h1>{networkConfig.label}</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="wrapper">
+                <div className="inner">
+                    <LandingEpochSection />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
