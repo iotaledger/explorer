a5fe19dc67c6ff02443113a7a1b256faa4064976
Mario Sarcevic <mario.sarcevic@iota.org>
2024-02-27 18:54:12 +0100

    chore: Improve nova address helper

--- client/src/helpers/nova/addressHelper.ts
+++ client/src/helpers/nova/addressHelper.ts
@@ -42,7 +42,7 @@ export class AddressHelper {
         }
 
         if (!bech32) {
-            // We assume this is hex and either use the hint or assume ed25519
+            // We assume this is hex
             hex = addressString;
             type = typeHint ?? AddressType.Ed25519;
             bech32 = this.computeBech32FromHexAndType(hex, type, hrp);
@@ -51,12 +51,12 @@ export class AddressHelper {
         return {
             bech32,
             hex: hex ? HexHelper.addPrefix(hex) : hex,
-            type,
+            type: type ?? typeHint,
             label: AddressHelper.typeLabel(type),
         };
     }
 
-    private static buildAddressFromTypes(address: Address, hrp: string, capabilities?: number[]): IAddressDetails {
+    private static buildAddressFromTypes(address: Address, hrp: string): IAddressDetails {
         let hex: string = "";
         let bech32: string = "";
 
@@ -74,14 +74,13 @@ export class AddressHelper {
             hex = (innerAddress as Ed25519Address).pubKeyHash;
         }
 
-        bech32 = this.computeBech32FromHexAndType(hex, address.type, hrp);
+        bech32 = Utils.addressToBech32(address, hrp);
 
         return {
             bech32,
             hex,
             type: address.type,
             label: AddressHelper.typeLabel(address.type),
-            capabilities,
         };
     }
 
@@ -95,8 +94,7 @@ export class AddressHelper {
         } else if (addressType === AddressType.Nft) {
             bech32 = Utils.nftIdToBech32(hex, hrp);
         } else if (addressType === AddressType.Anchor) {
-            // TODO Utils.anchorIdToBech32 does not exist https://github.com/iotaledger/iota-sdk/issues/1973
-            bech32 = Utils.accountIdToBech32(hex, hrp);
+            bech32 = Utils.anchorIdToBech32(hex, hrp);
         } else if (addressType === AddressType.ImplicitAccountCreation) {
             bech32 = Utils.hexToBech32(hex, hrp);
         }
@@ -109,7 +107,7 @@ export class AddressHelper {
      * @param addressType The address type to get the label for.
      * @returns The label.
      */
-    private static typeLabel(addressType?: number): string | undefined {
+    private static typeLabel(addressType?: AddressType): string | undefined {
         if (addressType === AddressType.Ed25519) {
             return "Ed25519";
         } else if (addressType === AddressType.Account) {
@@ -118,6 +116,10 @@ export class AddressHelper {
             return "NFT";
         } else if (addressType === AddressType.Anchor) {
             return "Anchor";
+        } else if (addressType === AddressType.ImplicitAccountCreation) {
+            return "Implicit Account Creation";
         }
+
+        return "Unknown";
     }
 }
