ea21b5d67769bf1e8c493f779ec232fcc116f75a
JCNoguera <jcnoguera@boxfish.studio>
2024-02-28 12:03:31 +0100

    chore: rename visualizer component to NovaVisualizer

--- client/src/features/visualizer-threejs/NovaVisualizer.tsx
+++ client/src/features/visualizer-threejs/NovaVisualizer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useGetThemeMode } from "~/helpers/hooks/useGetThemeMode.js";
 import { VisualizerRouteProps } from "~/app/routes/VisualizerRouteProps.js";
 import { RouteComponentProps } from "react-router-dom";
-import NovaVisualizer from "./VisualizerInstance";
+import VisualizerInstance from "./VisualizerInstance";
 
-export default function NovaVisualizerWrapper(props: RouteComponentProps<VisualizerRouteProps>): React.JSX.Element {
+export default function NovaVisualizer(props: RouteComponentProps<VisualizerRouteProps>): React.JSX.Element {
     const theme = useGetThemeMode();
 
-    return <NovaVisualizer key={theme} {...props} />;
+    return <VisualizerInstance key={theme} {...props} />;
 }
