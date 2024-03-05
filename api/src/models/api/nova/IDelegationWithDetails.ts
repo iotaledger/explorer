758aca474ea2942f61f9f23fb9432bbfc8904ef4
Branko Bosnic <brankobosnic1@gmail.com>
2024-03-05 09:49:24 +0100

    fix: lint errors

--- api/src/models/api/nova/IDelegationWithDetails.ts
+++ api/src/models/api/nova/IDelegationWithDetails.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { OutputWithMetadataResponse } from "@iota/sdk-nova";
 import { IRewardsResponse } from "./IRewardsResponse";
 
