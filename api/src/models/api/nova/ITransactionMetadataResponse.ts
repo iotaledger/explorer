ecbf520a3a9b0a732e825328eab27c5234913a6d
Mario Sarcevic <mario.sarcevic@iota.org>
2024-03-08 14:20:56 +0100

    chore: Add eslint ignore import unresolved

--- api/src/models/api/nova/ITransactionMetadataResponse.ts
+++ api/src/models/api/nova/ITransactionMetadataResponse.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 import { TransactionMetadataResponse } from "@iota/sdk-nova";
 import { IResponse } from "./IResponse";
 
