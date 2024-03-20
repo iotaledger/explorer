937760a3fdc257d1156c85a9b2b04fa03bd7b195
Bran <52735957+brancoder@users.noreply.github.com>
2024-03-20 17:58:51 +0100

    Feat: Add Metrics to Epoch Page (#1291)
    * feat: add metrics to epoch page
    
    * fix: revert changes to tsconfig file
    
    * feat: update epoch queries by epoch index
    
    * fix: resolve comments
    
    * fix: geting epoch stats from cache
    
    * fix: lint error
    
    * fix: protocol params and lint errors
    
    * feat: Hide validators table if no date (epoch page)
    
    * chore: Silence lint errors CI related
    
    ---------
    
    Co-authored-by: Mario Sarcevic <mario.sarcevic@iota.org>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

---
+++ client/src/helpers/nova/hooks/useEpochStats.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { useIsMounted } from "~helpers/hooks/useIsMounted";
+import { ServiceFactory } from "~factories/serviceFactory";
+import { NOVA } from "~models/config/protocolVersion";
+import { NovaApiClient } from "~/services/nova/novaApiClient";
+import { IEpochAnalyticStats } from "~/models/api/nova/stats/IEpochAnalyticStats";
+
+/**
+ * Fetch the epoch stats
+ * @param network The Network in context
+ * @param epochIndex The epoch index
+ * @returns The epoch stats and a loading bool.
+ */
+export function useEpochStats(network: string, epochIndex: string | null): [IEpochAnalyticStats | null, boolean] {
+    const isMounted = useIsMounted();
+    const [apiClient] = useState(ServiceFactory.get<NovaApiClient>(`api-client-${NOVA}`));
+    const [epochStats, setEpochStats] = useState<IEpochAnalyticStats | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        setEpochStats(null);
+        if (epochIndex) {
+            // eslint-disable-next-line no-void
+            void (async () => {
+                apiClient
+                    .epochStats({
+                        network,
+                        epochIndex,
+                    })
+                    .then((response) => {
+                        if (isMounted) {
+                            setEpochStats(response ?? null);
+                        }
+                    })
+                    .finally(() => {
+                        setIsLoading(false);
+                    });
+            })();
+        } else {
+            setIsLoading(false);
+        }
+    }, [network, epochIndex]);
+
+    return [epochStats, isLoading];
+}
