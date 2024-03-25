a19313916b2182014721ede9914f2194210583df
Mario Sarcevic <mario.sarcevic@iota.org>
2024-03-25 13:30:29 +0100

    feat: Make circulating supply update every 10 minutes

--- api/src/services/stardust/nodeInfoService.ts
+++ api/src/services/stardust/nodeInfoService.ts
@@ -1,9 +1,13 @@
 import { INodeInfoBaseToken, IRent, Client } from "@iota/sdk";
+import cron from "node-cron";
 import { StardustApiService } from "./stardustApiService";
 import { NodeInfoError } from "../../errors/nodeInfoError";
 import { ServiceFactory } from "../../factories/serviceFactory";
 import logger from "../../logger";
 import { INetwork } from "../../models/db/INetwork";
+
+// The cron interval value to update the circulating supply every 10 minutes.
+const CIRCULATING_SUPPLY_UPDATE_INTERVAL = "*/10 * * * *";
 
 /**
  * The reduced node info fields relevant for Explorer.
@@ -50,12 +54,12 @@ export class NodeInfoService {
      * Create a new instance of NodeInfoService.
      * @param network The network config.
      * @param nodeInfo The fetched node info.
-     * @param circulatingSupply The circulating supply of the network.
      */
-    private constructor(network: INetwork, nodeInfo: IReducedNodeInfo, circulatingSupply?: number | null) {
+    private constructor(network: INetwork, nodeInfo: IReducedNodeInfo) {
         this._network = network;
         this._nodeInfo = nodeInfo;
-        this._ciruclatingSupply = circulatingSupply ?? null;
+
+        this.setupCirculatingSupplyUpdater();
     }
 
     public get circulatingSupply() {
@@ -64,38 +68,51 @@ export class NodeInfoService {
 
     public static async build(network: INetwork): Promise<NodeInfoService> {
         const apiClient = ServiceFactory.get<Client>(`client-${network.network}`);
-        const stardustApiService = ServiceFactory.get<StardustApiService>(`api-service-${network.network}`);
-        let nodeInfo: IReducedNodeInfo;
-        let circulatingSupply: number | null = null;
 
         try {
             const response = await apiClient.getInfo();
-            nodeInfo = {
+            const nodeInfo = {
                 baseToken: response.nodeInfo.baseToken,
                 protocolVersion: response.nodeInfo.protocol.version,
                 bech32Hrp: response.nodeInfo.protocol.bech32Hrp,
                 rentStructure: response.nodeInfo.protocol.rentStructure,
             };
+
+            return new NodeInfoService(network, nodeInfo);
         } catch (err) {
             throw new NodeInfoError(`Failed to fetch node info for "${network.network}" with error:\n${err}`);
         }
+    }
+
+    public getNodeInfo(): IReducedNodeInfo {
+        return this._nodeInfo;
+    }
+
+    private setupCirculatingSupplyUpdater() {
+        // eslint-disable-next-line no-void
+        void this.updateCirculatingSupply();
+
+        cron.schedule(CIRCULATING_SUPPLY_UPDATE_INTERVAL, async () => {
+            await this.updateCirculatingSupply();
+        });
+    }
+
+    private async updateCirculatingSupply() {
+        const stardustApiService = ServiceFactory.get<StardustApiService>(`api-service-${this._network.network}`);
+        let circulatingSupply: number | null = null;
 
         try {
             const circulatingSupplyInBaseToken = await stardustApiService.circulatingSupply();
             if (circulatingSupplyInBaseToken) {
                 // The circulating supply from inx-supply-tracker is returned in base token,
                 // so we format it to subunit
-                circulatingSupply = circulatingSupplyInBaseToken * Math.pow(10, nodeInfo.baseToken.decimals);
-                logger.debug(`[NodeInfoService] Circulating supply for ${network.network} (in subunit): ${circulatingSupply}`);
+                circulatingSupply = circulatingSupplyInBaseToken * Math.pow(10, this._nodeInfo.baseToken.decimals);
+                logger.debug(`[NodeInfoService] Circulating supply for ${this._network.network} (in subunit): ${circulatingSupply}`);
+
+                this._ciruclatingSupply = circulatingSupply;
             }
         } catch {
-            logger.debug(`[NodeInfoService] Failed fetching circulating supply for ${network.network}`);
+            logger.debug(`[NodeInfoService] Failed fetching circulating supply for ${this._network.network}`);
         }
-
-        return new NodeInfoService(network, nodeInfo, circulatingSupply);
-    }
-
-    public getNodeInfo(): IReducedNodeInfo {
-        return this._nodeInfo;
     }
 }
