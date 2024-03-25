f7f487394f959b8e23fc6fc1cfd4c98dc64012e5
Mario Sarcevic <mario.sarcevic@iota.org>
2024-03-25 13:04:37 +0100

    feat: Use circulating supply from inx-supply-tracker in stardust networks if available (fallback to network config value)

--- api/src/services/stardust/nodeInfoService.ts
+++ api/src/services/stardust/nodeInfoService.ts
@@ -1,6 +1,8 @@
 import { INodeInfoBaseToken, IRent, Client } from "@iota/sdk";
+import { StardustApiService } from "./stardustApiService";
 import { NodeInfoError } from "../../errors/nodeInfoError";
 import { ServiceFactory } from "../../factories/serviceFactory";
+import logger from "../../logger";
 import { INetwork } from "../../models/db/INetwork";
 
 /**
@@ -40,31 +42,57 @@ export class NodeInfoService {
     protected _nodeInfo: IReducedNodeInfo;
 
     /**
+     * The circulating supply of the network if available.
+     */
+    protected _ciruclatingSupply?: number | null;
+
+    /**
      * Create a new instance of NodeInfoService.
      * @param network The network config.
-     * @param nodeInfo The fetched node info
+     * @param nodeInfo The fetched node info.
+     * @param circulatingSupply The circulating supply of the network.
      */
-    private constructor(network: INetwork, nodeInfo: IReducedNodeInfo) {
+    private constructor(network: INetwork, nodeInfo: IReducedNodeInfo, circulatingSupply?: number | null) {
         this._network = network;
         this._nodeInfo = nodeInfo;
+        this._ciruclatingSupply = circulatingSupply ?? null;
+    }
+
+    public get circulatingSupply() {
+        return this._ciruclatingSupply;
     }
 
     public static async build(network: INetwork): Promise<NodeInfoService> {
         const apiClient = ServiceFactory.get<Client>(`client-${network.network}`);
+        const stardustApiService = ServiceFactory.get<StardustApiService>(`api-service-${network.network}`);
+        let nodeInfo: IReducedNodeInfo;
+        let circulatingSupply: number | null = null;
 
         try {
             const response = await apiClient.getInfo();
-            const nodeInfo = {
+            nodeInfo = {
                 baseToken: response.nodeInfo.baseToken,
                 protocolVersion: response.nodeInfo.protocol.version,
                 bech32Hrp: response.nodeInfo.protocol.bech32Hrp,
                 rentStructure: response.nodeInfo.protocol.rentStructure,
             };
-
-            return new NodeInfoService(network, nodeInfo);
         } catch (err) {
             throw new NodeInfoError(`Failed to fetch node info for "${network.network}" with error:\n${err}`);
         }
+
+        try {
+            const circulatingSupplyInBaseToken = await stardustApiService.circulatingSupply();
+            if (circulatingSupplyInBaseToken) {
+                // The circulating supply from inx-supply-tracker is returned in base token,
+                // so we format it to subunit
+                circulatingSupply = circulatingSupplyInBaseToken * Math.pow(10, nodeInfo.baseToken.decimals);
+                logger.debug(`[NodeInfoService] Circulating supply for ${network.network} (in subunit): ${circulatingSupply}`);
+            }
+        } catch {
+            logger.debug(`[NodeInfoService] Failed fetching circulating supply for ${network.network}`);
+        }
+
+        return new NodeInfoService(network, nodeInfo, circulatingSupply);
     }
 
     public getNodeInfo(): IReducedNodeInfo {
