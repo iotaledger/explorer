3278cf1c1e20b7432eaa2e1c340a55adc0e2411b
Eugene P <panteleymonchuk@gmail.com>
2024-03-27 12:06:22 +0200

    fix: nova visualizer not loading when switching networks (#1321)
    * feat: switching network
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * feat: nova visualizer loading when switching networks
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * fix: nova visualizer not loading when switching networks. Add changes request.
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    * fix: changes for PR 1335
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    
    ---------
    
    Signed-off-by: Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- client/src/features/visualizer-vivagraph/hooks/useFeed.ts
+++ client/src/features/visualizer-vivagraph/hooks/useFeed.ts
@@ -38,6 +38,7 @@ export const useFeed = (network: string) => {
     const addToConfirmedBlocksBySlot = useTangleStore((state) => state.addToConfirmedBlocksBySlot);
     const removeConfirmedBlocksSlot = useTangleStore((state) => state.removeConfirmedBlocksSlot);
     const confirmedBlocksBySlot = useTangleStore((state) => state.confirmedBlocksBySlot);
+    const resetTangleStore = useTangleStore((state) => state.resetTangleStore);
     const themeMode = useGetThemeMode();
 
     const graphContext = useContext(GraphContext);
@@ -56,6 +57,13 @@ export const useFeed = (network: string) => {
         }
         handleHighlight(selectedNode?.blockId, search);
     }, [graphContext.isVivaReady, selectedNode, search]);
+
+    useEffect(() => {
+        return () => {
+            resetTangleStore();
+            feedService.unsubscribeBlocks();
+        };
+    }, []);
 
     function handleHighlight(selectedNodeId?: string, search?: string) {
         resetHighlight();
@@ -259,32 +267,32 @@ export const useFeed = (network: string) => {
     }
 
     function onSlotFinalized(slotFinalized: SlotIndex): void {
-        const slotsBefore = Array.from(confirmedBlocksBySlot.keys());
+        const previousFinalizedSlots = Array.from(confirmedBlocksBySlot.keys());
+        const allFinalizedSlots = [...previousFinalizedSlots, slotFinalized];
+        const confirmedBlocks = [];
 
-        const slots = [...slotsBefore, slotFinalized];
+        for (const slot of allFinalizedSlots) {
+            const confirmedBlockIds = confirmedBlocksBySlot.get(slot);
 
-        const blocks = [];
-        for (const slot of slots) {
-            const blockIds = confirmedBlocksBySlot.get(slot);
-            if (blockIds) {
-                blocks.push(...blockIds);
+            if (confirmedBlockIds) {
+                confirmedBlocks.push(...confirmedBlockIds);
             }
         }
 
-        if (blocks?.length) {
-            blocks.forEach((blockId) => {
-                const selectedColor = getBlockColorByState(themeMode, "finalized");
-                if (selectedColor) {
+        if (confirmedBlocks?.length) {
+            confirmedBlocks.forEach((blockId) => {
+                const finalizedBlockColor = getBlockColorByState(themeMode, "finalized");
+                if (finalizedBlockColor) {
                     updateBlockIdToMetadata(blockId, {
                         state: "finalized",
-                        color: selectedColor,
+                        color: finalizedBlockColor,
                     });
-                    updateBlockColor(blockId, selectedColor);
+                    updateBlockColor(blockId, finalizedBlockColor);
                 }
             });
         }
 
-        for (const slot of slots) {
+        for (const slot of allFinalizedSlots) {
             removeConfirmedBlocksSlot(slot);
         }
     }
