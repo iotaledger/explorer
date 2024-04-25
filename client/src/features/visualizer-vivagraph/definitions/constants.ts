b4f06473c06d143a1b07b9a9c4160f44d5a3240e
JCNoguera <88061365+VmMad@users.noreply.github.com>
2024-04-25 15:10:22 +0200

    feat: improve block colors in visualizer nova (#1436)
    * feat: improve block colors in visualizer nova
    
    * fix: remove whitespace
    
    * empty-commit
    
    * styles: improve status pill by adding a nova success color
    
    * chore: revert variables
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- client/src/features/visualizer-vivagraph/definitions/constants.ts
+++ client/src/features/visualizer-vivagraph/definitions/constants.ts
@@ -4,7 +4,8 @@ import { ThemeMode } from "./enums";
 export const MAX_VISIBLE_BLOCKS = 2500;
 
 // colors
-export const ACCEPTED_BLOCK_COLORS: string[] = ["#0101FF", "#0000DB", "#0101AB"];
+export const PENDING_BLOCK_COLOR = "#5C84FA";
+export const ACCEPTED_BLOCK_COLOR = "#C026D3";
 export const CONFIRMED_BLOCK_COLOR = "#3CE5E1";
 export const ORPHANED_BLOCK_COLOR = "#C026D3";
 export const DROPPED_BLOCK_COLOR = ORPHANED_BLOCK_COLOR;
@@ -18,23 +19,22 @@ export const EDGE_COLOR_BEFORE: number = 0x0000ff55;
 // colors by theme
 export const PENDING_BLOCK_COLOR_LIGHTMODE = "#A6C3FC";
 export const PENDING_BLOCK_COLOR_DARKMODE = "#5C84FA";
-export const FINALIZED_BLOCK_COLOR_LIGHTMODE = "#5C84FA";
-export const FINALIZED_BLOCK_COLOR_DARKMODE = "#000081";
+export const FINALIZED_BLOCK_COLOR = "#0000DB";
 
 export const THEME_BLOCK_COLORS: Record<ThemeMode, Record<BlockState, string | string[]>> = {
     [ThemeMode.Dark]: {
-        accepted: ACCEPTED_BLOCK_COLORS,
-        pending: PENDING_BLOCK_COLOR_DARKMODE,
+        accepted: ACCEPTED_BLOCK_COLOR,
+        pending: PENDING_BLOCK_COLOR,
         confirmed: CONFIRMED_BLOCK_COLOR,
-        finalized: FINALIZED_BLOCK_COLOR_DARKMODE,
+        finalized: FINALIZED_BLOCK_COLOR,
         dropped: DROPPED_BLOCK_COLOR,
         orphaned: ORPHANED_BLOCK_COLOR,
     },
     [ThemeMode.Light]: {
-        accepted: ACCEPTED_BLOCK_COLORS,
-        pending: PENDING_BLOCK_COLOR_LIGHTMODE,
+        accepted: ACCEPTED_BLOCK_COLOR,
+        pending: PENDING_BLOCK_COLOR,
         confirmed: CONFIRMED_BLOCK_COLOR,
-        finalized: FINALIZED_BLOCK_COLOR_LIGHTMODE,
+        finalized: FINALIZED_BLOCK_COLOR,
         dropped: DROPPED_BLOCK_COLOR,
         orphaned: ORPHANED_BLOCK_COLOR,
     },
