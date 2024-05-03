622004298feedd2d381420d632e8021cbf0fa5ce
Branko Bosnic <brankobosnic1@gmail.com>
2024-05-03 14:45:44 +0200

    fix: delegations section

--- client/src/app/components/nova/address/section/delegation/DelegationSection.tsx
+++ client/src/app/components/nova/address/section/delegation/DelegationSection.tsx
@@ -1,10 +1,14 @@
-import { DelegationOutput, Utils } from "@iota/sdk-wasm-nova/web";
 import React, { useEffect, useState } from "react";
 import Pagination from "~/app/components/Pagination";
-import TruncatedId from "~/app/components/stardust/TruncatedId";
 import { useNetworkInfoNova } from "~/helpers/nova/networkInfo";
 import "./DelegationSection.scss";
 import { IDelegationWithDetails } from "~/models/api/nova/IDelegationWithDetails";
+import { CardInfo, CardInfoProps } from "~/app/components/CardInfo";
+import { formatAmount } from "~/helpers/stardust/valueFormatHelper";
+import { DelegationsTableHeadings } from "~/app/lib/ui/enums/DelegationsTableHeadings.enum";
+import { useGenerateDelegationsTable } from "~/helpers/nova/hooks/useGenerateDelegationsTable";
+import TableCellWrapper, { TTableData } from "../../../TableCell";
+import Table, { ITableRow } from "~/app/components/Table";
 
 interface DelegationSectionProps {
     readonly delegationDetails: IDelegationWithDetails[] | null;
@@ -13,9 +17,12 @@ interface DelegationSectionProps {
 const PAGE_SIZE: number = 10;
 
 const DelegationSection: React.FC<DelegationSectionProps> = ({ delegationDetails }) => {
-    const { name: network, bech32Hrp } = useNetworkInfoNova((s) => s.networkInfo);
+    const { tokenInfo, manaInfo } = useNetworkInfoNova((s) => s.networkInfo);
+    const [isFormatBalance, setIsFormatBalance] = useState<boolean>(false);
     const [pageNumber, setPageNumber] = useState(1);
     const [currentPage, setCurrentPage] = useState<IDelegationWithDetails[]>([]);
+    const tableData: ITableRow<TTableData>[] = useGenerateDelegationsTable(currentPage ?? []);
+    const tableHeadings = Object.values(DelegationsTableHeadings);
 
     const totalAmount = delegationDetails?.reduce((acc, delegation) => acc + BigInt(delegation.output?.output?.amount ?? 0), BigInt(0));
     const totalRewards = delegationDetails?.reduce(
@@ -35,87 +42,40 @@ const DelegationSection: React.FC<DelegationSectionProps> = ({ delegationDetails
         return null;
     }
 
+    const delegationData: CardInfoProps[] = [
+        {
+            title: "Total amount:",
+            value: formatAmount(totalAmount ?? 0, tokenInfo, isFormatBalance),
+            onClickValue: () => setIsFormatBalance(!isFormatBalance),
+            copyValue: String(totalAmount),
+        },
+        {
+            title: "Total rewards",
+            value: formatAmount(totalRewards ?? 0, manaInfo, isFormatBalance),
+            onClickValue: () => setIsFormatBalance(!isFormatBalance),
+            copyValue: String(totalRewards),
+        },
+    ];
     return (
-        <div className="section transaction--section">
-            <div className="card card--no-border">
-                <div className="field">
-                    <div className="card--label margin-b-t">Total amount</div>
-                    <div className="card--value">{totalAmount?.toString()}</div>
-                </div>
-                <div className="field">
-                    <div className="card--label margin-b-t">Total rewards</div>
-                    <div className="card--value">{totalRewards?.toString()}</div>
-                </div>
-            </div>
-
-            <table className="table--delegation">
-                <thead>
-                    <tr>
-                        <th>Output Id</th>
-                        <th>Validator address</th>
-                        <th>Amount</th>
-                        <th>Rewards</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {currentPage.map((delegation, k) => {
-                        const outputId = delegation.output.metadata.outputId;
-                        const validatorAddress = Utils.addressToBech32(
-                            (delegation.output?.output as DelegationOutput).validatorAddress,
-                            bech32Hrp,
-                        );
-
-                        return (
-                            <tr key={`delegation-row-${k}`}>
-                                <td className="highlight">
-                                    <TruncatedId id={outputId} link={`/${network}/output/${outputId}`} />
-                                </td>
-                                <td className="highlight">
-                                    <TruncatedId id={validatorAddress} link={`/${network}/addr/${validatorAddress}`} />
-                                </td>
-                                <td>{delegation.output?.output.amount.toString() ?? "-"}</td>
-                                <td>{delegation.rewards?.manaRewards?.rewards.toString() ?? "-"}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-
-            {/* Only visible in mobile*/}
-            <div className="cards--delegation">
-                {currentPage.map((delegation, k) => {
-                    const outputId = delegation.output.metadata.outputId;
-                    const validatorAddress = Utils.addressToBech32(
-                        (delegation.output?.output as DelegationOutput).validatorAddress,
-                        bech32Hrp,
-                    );
-
+        <div className="section section--delegations">
+            <div className="card-info-wrapper">
+                {delegationData.map((data, index) => {
                     return (
-                        <div className="card card--delegation" key={`delegation-row-${k}`}>
-                            <div className="field">
-                                <div className="card--label">Output Id</div>
-                                <div className="card--value">
-                                    <TruncatedId id={outputId} link={`/${network}/output/${outputId}`} />
-                                </div>
-                            </div>
-                            <div className="field">
-                                <div className="card--label">Validator Address</div>
-                                <div className="card--value">
-                                    <TruncatedId id={validatorAddress} link={`/${network}/addr/${validatorAddress}`} />
-                                </div>
-                            </div>
-                            <div className="field">
-                                <div className="card--label">Amount</div>
-                                <div className="card--value">{delegation.output?.output.amount.toString() ?? "-"}</div>
-                            </div>
-                            <div className="field">
-                                <div className="card--label">Rewards</div>
-                                <div className="card--value">{delegation.rewards?.manaRewards?.rewards.toString() ?? "-"}</div>
-                            </div>
-                        </div>
+                        <CardInfo
+                            key={index}
+                            title={data.title}
+                            value={data.value}
+                            onClickValue={data.onClickValue}
+                            copyValue={data.copyValue}
+                        />
                     );
                 })}
             </div>
+
+            <div className="section--data delegation-outputs">
+                <Table headings={tableHeadings} data={tableData} TableDataComponent={TableCellWrapper} />
+            </div>
+
             <Pagination
                 currentPage={pageNumber}
                 totalCount={delegationDetails?.length ?? 0}
